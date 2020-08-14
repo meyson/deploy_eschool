16559a91d3e4dcf4af7a78afa199f1fc0871006b
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-08-14 22:36:54 +0300

    circleci deploy

--- config_files/be_watcher.sh
+++ config_files/be_watcher.sh
@@ -1,29 +1,46 @@
 #!/usr/bin/env bash
-# TODO root > /home/user
-WATCH_FILE="/root/eschool.jar"
 
-PSID=""
+app_dir="/opt/eschool"
+app_path="$app_dir/eschool.jar"
+artifact_url="eschool_url.txt"
+
+APP_PSID=""
+
 run_command() {
-  java -jar /root/eschool.jar -Dspring.profiles.active=production &
-  PSID=$!
-  echo "Running! $(date)"
+  java -jar "$app_path" -Dspring.profiles.active=production &
+  APP_PSID=$!
+  echo "Running! $(date) psid: $PSID"
 }
 
-reload_process() {
-  kill $PSID || true
+restart_app() {
+  local url=$(cat "$app_dir/$artifact_url")
+  local app_path_tmp="$app_path.tmp"
+  # download new artifact
+  wget "$url" -O "$app_path_tmp"
+  kill $APP_PSID || true
+  # replace original artifact
+  rm "$app_path"
+  mv "$app_path_tmp" "$app_path"
   run_command
 }
 
-# start program
-run_command
+main() {
+  # start program
+  run_command
 
-# todo
-WHEN=$(stat -c "%Y" $WATCH_FILE || true)
-while true; do
-  NOW=$(stat -c "%Y" $WATCH_FILE || true)
-  if [ "$NOW" -gt "$WHEN" ]; then
-    reload_process
-  fi
-  WHEN="$NOW"
-  sleep 1
-done;
+  # reload app when we deploy (change) file
+  inotifywait -e close_write,moved_to,create -m "$app_dir" |
+  while read -r directory events filename; do
+    if [ "$filename" = "$artifact_url" ]; then
+      restart_app
+    fi
+  done
+}
+
+# https://stackoverflow.com/questions/1715137/what-is-the-best-way-to-ensure-only-one-instance-of-a-bash-script-is-running/17480020
+# run only one instance of this script
+(
+  flock -n 9 || exit 1
+  main "$@"
+) 9>/tmp/be_watcher.lock
+
--- config_files/deploy.py
+++ config_files/deploy.py
@@ -1,15 +1,16 @@
 import argparse
-import requests
-from pathlib import Path
-import shutil
-import paramiko
 import os
+import shutil
+from pathlib import Path
+
+import paramiko
+import requests
 
 SSH_USER = os.environ['USER']
-API_KEY = Path(f'/home/{SSH_USER}/.circlecitoken').read_text()[:-1]
+API_KEY = Path(f'/home/{SSH_USER}/.circlecitoken').read_text().strip()
 
 parser = argparse.ArgumentParser(description='Deploy eschool')
-parser.add_argument('-j', "--job", default='', type=str, help='job number')
+parser.add_argument('-j', "--job", default='', type=int, help='job number')
 
 args = parser.parse_args()
 
@@ -28,11 +29,9 @@ def get_artifact(project_slug, job_number="latest"):
         'Accept': 'application/json',
         'Circle-Token': f'{API_KEY}'
     }
-
     r = requests.get(
         f'https://circleci.com/api/v2/project/{project_slug}/{job_number}/artifacts',
         params={}, headers=headers)
-
     return r.json()
 
 
@@ -40,13 +39,18 @@ def deploy_artifacts(server, artifacts):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, username=SSH_USER)
+    client.connect(hostname=server,
+                   username=SSH_USER,
+                   timeout=4)
 
     for artifact in artifacts:
         url = artifact['url']
         print(f'Deploying {url} to {server}')
-        command = f'wget {url} -P /home/{SSH_USER}/'
+        # send artifact url to server
+        command = f'sudo bash -c "echo "{url}" > /opt/eschool/eschool_url.txt"'
         stdin, stdout, stderr = client.exec_command(command)
+        for line in stderr:
+            print('... ' + line.strip('\n'))
         for line in stdout:
             print('... ' + line.strip('\n'))
         client.close()
@@ -54,8 +58,10 @@ def deploy_artifacts(server, artifacts):
 
 
 def main():
+    # FIXME
     servers = ['10.156.0.50', '10.156.0.51']
-    artifacts = get_artifact('github/meyson/eSchool', job_number=args.job)['items']
+    project_slug = 'github/meyson/eSchool'
+    artifacts = get_artifact(project_slug, job_number=args.job)['items']
     print(artifacts)
     for server in servers:
         try:
